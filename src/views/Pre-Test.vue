cf86c7a7feae533e229d9c3013af908f5289bccf
Nikoloz97 <nick.gotsy@gmail.com>
2022-12-20 18:29:36 -0500

    Fixed card layout

--- src/views/Pre-Test.vue
+++ src/views/Pre-Test.vue
@@ -1,6 +1,5 @@
 <template>
 <div>
-    <!-- TODO: 2 cards per row (flex-wrap + set a card width) -->
     <!-- TODO: Create an object list of content for each card in the store, and create a v-for + v-bind for each card -->
 <b-container class="d-flex justify-content-around flex-wrap">
         <b-card title="Question 1"
@@ -8,107 +7,29 @@
                 img-alt="Amino acid"
                 img-top 
                 style="width: 40%; height: 45rem"
-                class="mb-2 shadow">
+                class="mb-5 shadow">
             <b-card-text>
                 <p>What is this amino acid?</p>
             </b-card-text>
-                <b-container class="border d-flex">
-                    <b-row class="justify-content-around" style="height: 10rem">
-                        <b-button class="btn-md mt-4"
-                                    style="height: 3rem"
-                                    variant="primary"
-                                    >Cysteine</b-button>
-                        <b-button class="btn-md mt-4"
-                                    style="height: 3rem"
-                                    variant="primary"
-                                    mt-4>Alanine</b-button>
-                    </b-row>
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    style="height: 3rem"
-                                    variant="primary">Arginine</b-button>
-                        <b-button class="btn-md mt-4"
-                                  variant="primary">Histidine</b-button>
-                    </b-row>
-                </b-container>
-        </b-card>
+                <b-container class="border d-flex justify-content-around flex-wrap" style=" height: 60%;">
+                        <b-col>
+                            <b-button class="mt-3"
+                                        style="height: 30%; width: 80%;"
+                                        variant="primary">Cysteine</b-button>
+                            <b-button class="mt-3"
+                                        style="height: 30%; width: 80%;"
+                                        variant="primary">Alanine</b-button>
+                        </b-col>
+                
+                        <b-col>
+                            <b-button class="mt-3"
+                                        style="height: 30%; width: 80%;"
+                                        variant="primary">Arginine</b-button>
+                            <b-button class="mt-3"
+                                       style="height: 30%; width: 80%;"
+                                      variant="primary">Histidine</b-button>
+                        </b-col>
 
-        <b-card title="Question 1"
-                img-src="https://assets.fishersci.com/TFS-Assets/CCG/Chemical-Structures/chemical-structure-cas-56-87-1.jpg-650.jpg"
-                img-alt="Amino acid"
-                img-top style="max-width:25rem"
-                class="mb-2 shadow">
-            <b-card-text>
-                <p>What is this amino acid?</p>
-            </b-card-text>
-                <b-container class="border d-flex align-content-between" style="height: 10rem">
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    variant="primary"
-                                    >Cysteine</b-button>
-                        <b-button class="btn-md mt-4"
-                                    variant="primary"
-                                    mt-4>Alanine</b-button>
-                    </b-row>
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    variant="primary">Arginine</b-button>
-                        <b-button class="btn-md mt-4"
-                                  variant="primary">Histidine</b-button>
-                    </b-row>
-                </b-container>
-        </b-card>
-
-
-        <b-card title="Question 1"
-                img-src="https://assets.fishersci.com/TFS-Assets/CCG/Chemical-Structures/chemical-structure-cas-56-87-1.jpg-650.jpg"
-                img-alt="Amino acid"
-                img-top style="max-width:25rem"
-                class="mb-2 shadow">
-            <b-card-text>
-                <p>What is this amino acid?</p>
-            </b-card-text>
-                <b-container class="border d-flex align-content-between" style="height: 10rem">
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    variant="primary"
-                                    >Cysteine</b-button>
-                        <b-button class="btn-md mt-4"
-                                    variant="primary"
-                                    mt-4>Alanine</b-button>
-                    </b-row>
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    variant="primary">Arginine</b-button>
-                        <b-button class="btn-md mt-4"
-                                  variant="primary">Histidine</b-button>
-                    </b-row>
-                </b-container>
-        </b-card>
-
-        <b-card title="Question 1"
-                img-src="https://assets.fishersci.com/TFS-Assets/CCG/Chemical-Structures/chemical-structure-cas-56-87-1.jpg-650.jpg"
-                img-alt="Amino acid"
-                img-top style="max-width:25rem"
-                class="mb-2 shadow">
-            <b-card-text>
-                <p>What is this amino acid?</p>
-            </b-card-text>
-                <b-container class="border d-flex align-content-between" style="height: 10rem">
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    variant="primary"
-                                    >Cysteine</b-button>
-                        <b-button class="btn-md mt-4"
-                                    variant="primary"
-                                    mt-4>Alanine</b-button>
-                    </b-row>
-                    <b-row class="justify-content-around" style="height: 2rem">
-                        <b-button class="btn-md mt-4"
-                                    variant="primary">Arginine</b-button>
-                        <b-button class="btn-md mt-4"
-                                  variant="primary">Histidine</b-button>
-                    </b-row>
                 </b-container>
         </b-card>
 </b-container>
