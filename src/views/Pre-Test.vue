4eb1fe7a50f80e3c094a7894f168a363847cefda
Nikoloz97 <nick.gotsy@gmail.com>
2022-12-23 09:59:30 -0500

    New card renders after each attempt

--- src/views/Pre-Test.vue
+++ src/views/Pre-Test.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
 
-    <!-- TODO: Display correctMsg if user clicks on correct option (vice versa w incorrect) -->
-<b-container class="d-flex justify-content-around flex-wrap" v-for="question in $store.state.pretest" :key="question.title">
+    <!-- TODO: create a "continue button that renders the next card (updates Current Question)"-->
+<b-container class="d-flex justify-content-center" v-for="question, index in $store.state.pretest" :key="question.title">
         <b-card 
+                v-if="index == $store.state.currentQuestion"
                 :img-src="question.imgsrc"
                 :img-alt="question.imgalt"
                 img-top 
                 style="width: 40%; height: 45rem"
-                class="mb-5 shadow">
-            <b-card-text :id= "question.title">
+                class="mb-5 shadow"
+                :title="question.title">
+            <b-card-text>
+                <p> Current score: {{ $store.state.correctAttempts}}/{{$store.state.totalAttempts}}</p>
                 <p class="statement">{{ question.statement }}</p>
                 <p class="d-none correct">{{ $store.state.correctMsg }}</p>
                 <p class="d-none incorrect">{{ $store.state.incorrectMsg }}</p>
@@ -19,7 +22,7 @@
                             <!-- Vue = implicitly knows that index = index of item in array -->
                             <div v-for="(choice, index) in question.choices" :key="choice.name">
                                 <b-button  v-if="index >= 2"
-                                            @click="displayAnswers(question), displayMessage(question, $event)"
+                                            @click="displayAnswers(question), displayMessage(question, $event), updateCurrentQuestion()"
                                             :class="{'bg-success' : choice.correct && question.displayAnswers, 'font-weight-bolder' : choice.correct && question.displayAnswers,'disabled': !choice.correct && question.displayAnswers}"
                                             class="mt-3"
                                             style="height: 30%; width: 80%;"
@@ -30,7 +33,7 @@
                         <b-col>
                             <div v-for="(choice, index) in question.choices" :key="choice.name">
                                 <b-button v-if="index < 2"
-                                            @click="displayAnswers(question), displayMessage(question, $event)"
+                                            @click="displayAnswers(question), displayMessage(question, $event), updateCurrentQuestion()"
                                             :class="{'bg-success': choice.correct && question.displayAnswers, 'disabled': !choice.correct && question.displayAnswers}"
                                             class="mt-3"
                                             style="height: 30%; width: 80%;"
@@ -49,40 +52,31 @@
 
 <script>
 
-
-// TODO: Continue where you left off with quiz youtube video
-
-// const SetAnswer = e => {
-//     this.$store.state.pretest.value
-// }
-
 export default {
     computed: {
         // This is where you can create functions that grabs specific pieces of the state data
         // Downside = function cannot contain a parameter
 
-        updateScore() {
-            let score = 0
-            this.$store.state.pretest.value.map(card => {
-                if (card.selected == card.answer) {
-                    score++
-                }
-            })
-            return score
-    }
+        
 },
     methods: {
         displayAnswers(question) {
             this.$store.commit("DISPLAY_ANSWERS", question)
         },
         displayMessage(question, event) {
+
+            // First, update total score...
+
+            this.updateAttemptsScore()
+
+            // Get button element that was clicked on
             const answerChoice = event.target;
 
-            // Getting card-text element
-            console.log(answerChoice.parentElement.parentElement.parentElement.previousSibling)
-
+            // Get card-text element
             const cardTextElement = answerChoice.parentElement.parentElement.parentElement.previousSibling;
+
             
+            // If correct answer was picked, display correctMessage, increase correct score...
             if (answerChoice.textContent == question.answer) {
 
                 for (const child of cardTextElement.children) {
@@ -93,7 +87,10 @@ export default {
                         child.classList.remove("d-none")
                     }
                 }
+                this.updateCorrectScore()
             }
+
+            // If incorrect asnwer was picked, display incorrectMessage...
             else {
                 for (const child of cardTextElement.children) {
                     if (child.classList.contains("statement")) {
@@ -105,6 +102,15 @@ export default {
                 }
             }
         },
+        updateCorrectScore() {
+            this.$store.commit("UPDATE_CORRECT_SCORE")
+        },
+        updateAttemptsScore() {
+            this.$store.commit("UPDATE_ATTEMPTS_SCORE")
+        },
+        updateCurrentQuestion() {
+            this.$store.commit("UPDATE_CURRENT_QUESTION")
+        }
     }
 }
 
