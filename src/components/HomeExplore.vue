e90ecc0e178bb1aa721f77213fa4400f80919c25
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-04 23:48:21 -0500

    Fixed carousel image dimensions

---
+++ src/components/HomeExplore.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <b-row align-h="center" class="mt-5"> <h1>Explore</h1></b-row>
+            <b-row align-h="center">
+                <b-carousel
+                id="carousel-1"
+                ref="carousel"
+                :interval="0"
+                controls
+                no-animation
+                indicators
+                background=""
+                class="carousel"
+                >
+                    <!-- TODO: Hook (bind?) the data to an API-->
+                    <b-carousel-slide
+                    caption="First Slide"
+                    text="This is the first slide"
+                    img-src="https://biologydictionary.net/wp-content/uploads/2020/03/Essential-amino-acid.jpg"
+                    >
+                    <h1>Amino acids</h1>
+                    </b-carousel-slide>
+                    <b-carousel-slide
+                    caption="First Slide"
+                    text="This is the first slide"
+                    img-src="https://media.proprofs.com/images/QM/user_images/2503852/Glycolysis.jpg"
+                    >
+                    <h1>Amino acids</h1>
+                    </b-carousel-slide>
+                    <b-carousel-slide
+                    caption="First Slide"
+                    text="This is the first slide"
+                    img-src="https://alevelnotes.com/static/content_images/AminoAcidball-labels.png"
+                    >
+                    <h1>Amino acids</h1>
+                    </b-carousel-slide>
+                </b-carousel>
+            </b-row>
+
+    </div>
+
+</template>
+
+<script>
+// import blank from ''
+
+export default {
+    name: 'HomeExplorer',
+    components: {},
+    data() {
+        return {
+
+        }
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+
+  mounted()  {
+    // this.$refs.carousel.$children.forEach((child) => console.log(child.computedHeight = 100))
+  },
+};
+</script>
+
+<style>
+
+/* Overwriting the image-fluid properties (makes each sliding image different dimensions) */
+/* Downside = makes transitions weird (therefore, turned off animation) */
+img.img-fluid {
+    height: 200px !important;
+    width: 300px !important;
+}
+
+</style>
