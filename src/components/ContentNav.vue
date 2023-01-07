75b03089d996d5af6b10f9761d258e3d11f05617
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-07 08:05:08 -0500

    Created search input form on content nav

--- src/components/ContentNav.vue
+++ src/components/ContentNav.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
+        <!-- Navbar -->
+        <b-navbar>
+            <b-navbar-nav>
+                <b-button>Hide</b-button>
+            </b-navbar-nav>
+
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form >
+                    <b-form-input class="ml-auto col-6" placeholder="Search"></b-form-input>
+                    <b-button class="ml-1" size="sm" type="Submit">Search</b-button>
+                </b-nav-form>
+            </b-navbar-nav>
+        </b-navbar>
+
+        <!-- Main section -->
         <b-card>
             <b-card-header>
                 <h4>Chapter 1</h4>
