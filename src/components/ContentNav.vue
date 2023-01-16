dd1b1cf02f1b720c5fef5d6b98d12a19ac855467
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-16 08:39:52 -0500

    Created chapters list for content nav

--- src/components/ContentNav.vue
+++ src/components/ContentNav.vue
@@ -4,6 +4,7 @@
         <b-navbar >
             <b-navbar-nav >
                 <b-nav-form>
+                    <!-- TODO: Make search bar filter out chapters based on input-->
                     <!-- Regular version -->
                     <b-form-input   v-show="!isCompact"
                                     class="ml-5 col-6" 
@@ -20,28 +21,31 @@
         </b-navbar>
 
         <!-- Main section -->
-        <b-card :class="{compact_list_items: isCompact}">
+        <b-card v-for="chapter in Chapters"
+                :key="chapter.Number"
+                :class="{compact_list_items: isCompact}">
+
             <b-card-header>
                 <!-- toggling nav. Pressed.sync attribute = toggles boolean upon click-->
                 <b-button :pressed.sync="isCompact">
-                    <h4 :class="{compact_chapters: isCompact}">Chapter 1</h4>
+                    <h4 :class="{compact_chapters: isCompact}">{{ chapter.Number }}</h4>
                 </b-button>
             </b-card-header>
 
             <b-card-body>
-                <b-card-title >Amino Acids</b-card-title>
-                <b-card-text v-show="!isCompact">Learning the structure, behavior, and functionality of the essential Amino Acids</b-card-text>
+                <b-card-title >{{ chapter.Title }}</b-card-title>
+                <b-card-text v-show="!isCompact">{{ chapter.Description }}</b-card-text>
             </b-card-body>
 
-            <b-list-group>
-                <b-list-group-item> Structure </b-list-group-item>
-                <b-list-group-item> Behavior </b-list-group-item>
-                <b-list-group-item> Functionality </b-list-group-item>
+            <b-list-group v-for="chapterItem in chapter.ChapterItems" :key="chapterItem">
+                <b-list-group-item> {{ chapterItem}} </b-list-group-item>
             </b-list-group>
+
+        </b-card>      
 
 
             <!-- Main Section 2 (hard-coded) -->
-            <b-card-header>
+            <!-- <b-card-header>
                 <b-button :pressed.sync="isCompact">
                     <h4 :class="{compact_chapters: isCompact}">Chapter 2</h4>
                 </b-button>
@@ -56,10 +60,9 @@
                 <b-list-group-item> Structure </b-list-group-item>
                 <b-list-group-item> Behavior </b-list-group-item>
                 <b-list-group-item> Functionality </b-list-group-item>
-            </b-list-group>
+            </b-list-group> -->
             
 
-        </b-card>      
     
     
     </div>
@@ -74,10 +77,36 @@
         data() {
             return {
                 isCompact: false,
+                Chapters: [
+                    {
+                        Number: "Chapter 1",
+                        Title: "Amino Acids",
+                        Description: "Learning the structure, behavior, and functionality of the essential Amino Acids",
+                        ChapterItems: ["Structure", "Behavior", "Functionality"]
+                    },
+                    {
+                        Number: "Chapter 2",
+                        Title: "Amino Acids",
+                        Description: "Learning the structure, behavior, and functionality of the essential Amino Acids",
+                        ChapterItems: ["Structure", "Behavior", "Functionality"]
+                    }, 
+                    {
+                        Number: "Chapter 3",
+                        Title: "Amino Acids",
+                        Description: "Learning the structure, behavior, and functionality of the essential Amino Acids",
+                        ChapterItems: ["Structure", "Behavior", "Functionality"]
+
+                    }
+                ],
+                Search: "",
+
             }
     
         },
         computed: {
+            // filteredSearch: function() {
+            //     return 
+            // }
     
         },
         methods: {
