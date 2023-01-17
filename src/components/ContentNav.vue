33db1d7d3dfcf2d2038dbbe4445292914647bf03
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-16 23:04:58 -0500

    Created toggleable menu content nav button

--- src/components/ContentNav.vue
+++ src/components/ContentNav.vue
@@ -1,48 +1,49 @@
 <template>
-                <!-- TODO: Fix chapterItems (potential solution = splice string array and create one large string to search on) -->
 
     <div>
         <!-- Navbar -->
-        <b-navbar >
-            <b-navbar-nav >
-                    <!-- Regular version -->
-                    <b-form-input   v-show="!isCompact"
-                                    v-model="Search"
-                                    class="ml-5 col-6" 
-                                    placeholder="Search"></b-form-input>
-                    <b-button v-show="!isCompact" class="ml-1" size="sm" type="Submit">Search</b-button>
+        <b-card :class="{compact_card: isCompact}">
+            <b-navbar >
+                <b-navbar-nav >
+                        <!-- Regular version -->
+                        <b-form-input   v-show="!isCompact"
+                                        v-model="Search"
+                                        class="ml-4 col-6"
+                                        placeholder="Search"></b-form-input>
+                        <b-form-input   v-show="isCompact"
+                                        v-model="Search"
+                                        class="ml-2 col-6"
+                                        placeholder="Search"></b-form-input>
+                        <b-button variant="outline-secondary" class="ml-1" size="sm" type="Submit">Search</b-button>
+                        <!-- Compact version -->
+                        
+                </b-navbar-nav>
+            </b-navbar>
 
-                    <!-- Compact version -->
-                    <b-form-input   v-show="isCompact"
-                                    v-model="Search"
-                                    class="ml-1 col-3" 
-                                    ></b-form-input>
-                    <b-button v-show="isCompact" class="ml-1" size="sm" type="Submit">Search</b-button>
-            </b-navbar-nav>
-        </b-navbar>
+            <b-button  
+                       :pressed.sync="isCompact" 
+                       variant="outline-info"
+                       class="col-7 mb-2">Menu</b-button>
 
-        <!-- Main section -->
-        <b-card v-for="chapter in filteredChapters"
-                :key="chapter.Number"
-                :class="{compact_list_items: isCompact}">
-
-            <b-card-header>
-                <!-- toggling nav. Pressed.sync attribute = toggles boolean upon click-->
-                <b-button :pressed.sync="isCompact">
-                    <h4 :class="{compact_chapters: isCompact}">{{ chapter.Number }}</h4>
-                </b-button>
-            </b-card-header>
-
-            <b-card-body>
-                <b-card-title >{{ chapter.Title }}</b-card-title>
-                <b-card-text v-show="!isCompact">{{ chapter.Description }}</b-card-text>
-            </b-card-body>
-
-            <b-list-group v-for="chapterItem in chapter.ChapterItems" :key="chapterItem">
-                <b-list-group-item> {{ chapterItem}} </b-list-group-item>
-            </b-list-group>
-
-        </b-card>      
+            <!-- Main section -->
+            <b-card v-for="chapter in filteredChapters"
+                    :key="chapter.Number">
+                <b-card-header>
+                    <!-- toggling nav. Pressed.sync attribute = toggles boolean upon click-->
+                    <b-button 
+                               variant="outline-secondary" >
+                        <h4 :class="{compact_chapters: isCompact}">{{ chapter.Number }}</h4>
+                    </b-button>
+                </b-card-header>
+                <b-card-body>
+                    <b-card-title >{{ chapter.Title }}</b-card-title>
+                    <b-card-text v-show="!isCompact">{{ chapter.Description }}</b-card-text>
+                </b-card-body>
+                <b-list-group v-for="chapterItem in chapter.ChapterItems" :key="chapterItem">
+                    <b-list-group-item> {{ chapterItem}} </b-list-group-item>
+                </b-list-group>
+            </b-card>
+        </b-card>
 
     </div>
     </template>
@@ -88,10 +89,7 @@
                     // match method = matches search term with the title
                     return chapter.Title.toLowerCase().match(this.Search.toLowerCase()) 
                             || chapter.Description.toLowerCase().match(this.Search.toLowerCase())
-                            // Looping and filtering through each chapterItems = doesn't work...)
-                            // || chapter.ChapterItems.forEach(chapterItem => {
-                            //     chapterItem.toLowerCase().match(this.Search.toLowerCase())
-                            // })
+                            // toString = converts string array into one long string
                             || chapter.ChapterItems.toString().toLowerCase().match(this.Search.toLowerCase())
                 })
             }
@@ -106,6 +104,9 @@
     </script>
     
     <style>
+    .compact_card {
+        width: 65%
+    }
     .compact_nav {
         width: 50%;
     }
@@ -122,6 +123,7 @@
         font-size: 0.5rem;
     }
 
+    
     
     
     </style>
