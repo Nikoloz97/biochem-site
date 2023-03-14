75d8e6e3fc5a87952467a76ef0562a8cbaa5c724
Nikoloz97 <nick.gotsy@gmail.com>
2023-03-14 17:26:36 -0400

    Replaced store deck to savedDecks

--- src/components/FlashCards/Main.vue
+++ src/components/FlashCards/Main.vue
@@ -2,10 +2,19 @@
     <div>
         <!-- Progress bar -->
         <progress-bar :BarState="progressBar"/>
-        <b-card>
+        <div v-for="(flashcard, index) in flashcards"
+            :key="index" >
+            <b-card v-if="index == currentFlashcard"
+                    :img-src="flashcard.ImageURL"
+                    img-top
+                    style="width: 40%; height: 45rem"
+                    class="mb-5 shadow"
+                    :title=""
+            >
             
-
-        </b-card>
+            
+            </b-card>
+        </div>
 
     </div>
 
@@ -22,8 +31,9 @@ export default {
             progressBar: {
                 max: this.$store.state.flashcards.length,
                 value: 0,
-            }
-            
+            },
+            flashcards: this.$store.state.flashcards,
+            currentFlashcard: this.$store.state.currentFlashcard
         }
 
     },
