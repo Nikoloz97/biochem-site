71856eda2c8bbe575e9e77e06c7cf9a9e8af0686
Nikoloz97 <nick.gotsy@gmail.com>
2023-03-14 10:08:44 -0400

    Centered decks column

--- src/views/Flashcards.vue
+++ src/views/Flashcards.vue
@@ -1,14 +1,20 @@
 <template>
 
 <div>
-    <b-row>
-        <b-col cols = "4">
-            <decks/>
-        </b-col>
-        <b-col cols="8" >
-            <flashcard/>
-        </b-col>
-    </b-row>
+    <b-container class="vh-100">
+        <b-row class = "h-100">
+            <b-col class="h-50" cols = "4" align-self="center">
+                    <b-row align-v="center">
+                        <b-col><decks/></b-col>
+                    </b-row>
+            </b-col>
+            <b-col class="h-100" align-v="center" cols="8" >
+                <flashcard/>
+            </b-col>
+        </b-row>
+    </b-container>
+
+
 </div>
 
 </template>
